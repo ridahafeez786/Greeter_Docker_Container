1f6dcd170b35697fd52d19fde28952d3123b503d
ridahafeez786 <ridahafeez786@gmail.com>
2026-09-22 11:44:59 +0500

    Initial commit: add Python greeter app

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class GreeterHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = "Hello, my name is Rida!"
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+
+        html = f"<h1>{message}</h1>"
+        self.wfile.write(html.encode("utf-8"))
+
+server = HTTPServer(("0.0.0.0", 8080), GreeterHandler)
+
+print("Greeter app running on port 8080", flush=True)
+
+server.serve_forever()
