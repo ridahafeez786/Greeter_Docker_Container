8a6c1fdb58b41bb55e4225b80abec9d503f62c71
ridahafeez786 <ridahafeez786@gmail.com>
2026-09-22 14:28:34 +0500

    Change greeting to Hey

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class GreeterHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        message =  "Hello, my name is Rida!<br>Welcome to my Docker application!"
+        message = "Hey, my name is Rida!<br>Welcome to my Docker application!"
 
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
